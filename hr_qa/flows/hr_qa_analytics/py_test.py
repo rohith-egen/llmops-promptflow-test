a952cbe52c00c4890e4dda6a5f5dc7642e2c86a6
vikramadithya baddam <vikramadithya.baddam@springml.com>
2024-04-08 11:18:17 +0530

    added comments

--- hr_qa/flows/hr_qa_analytics/py_test.py
+++ hr_qa/flows/hr_qa_analytics/py_test.py
@@ -4,6 +4,7 @@ from promptflow import tool
 # The inputs section will change based on the arguments of the tool function, after you save the code
 # Adding type to arguments and return value will help the system show the types properly
 # Please update the function name/signature per need
+#added comments
 @tool
 def my_python_tool(input1: str) -> str:
     return 'hello ' + input1
